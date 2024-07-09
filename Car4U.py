1f23ce120717b7aab6286a5e87be75c7e623327f
nij96969 <nijakbari@gmail.com>
2024-07-09 18:29:04 +0530

    Car4U HybridModel Dataset

---
+++ Car4U.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import pandas as pd
+import ast
+from HybridModel import hybrid_model
+
+# Load the dataset
+cars_data = pd.read_csv(r"D:\Car4U\Dataset\cars_data_clean.csv")
+
+#Total features
+overall_features = [
+    'listed_price', 'myear', 'body', 'transmission', 'fuel', 'km',
+    'Engine Type', 'Length', 'Width', 'Height', 'Max Power Delivered', 'Color' ,
+    'model' , 'images'
+]
+# List of numerical and categorical features
+numerical_features = ['listed_price', 'myear', 'Length',]
+categorical_features = ['body', 'transmission', 'fuel', 'Color']
+
+# Get min and max values for numerical features
+numerical_ranges = {feature: (int(cars_data[feature].min()), int(cars_data[feature].max())) for feature in numerical_features}
+
+# Title of the app
+st.title("Feature Selector")
+
+# Initialize session state for input values
+if 'inputs' not in st.session_state:
+    st.session_state.inputs = {
+        'listed_price': (numerical_ranges['listed_price'][0] + numerical_ranges['listed_price'][1]) // 2,
+        **{feature: (numerical_ranges[feature][0] + numerical_ranges[feature][1]) // 2 for feature in numerical_features if feature not in ['listed_price', 'km']},
+        **{feature: cars_data[feature].unique().tolist()[0] for feature in categorical_features}
+    }
+
+with st.form(key="feature"):
+    # Inputs for listed_price and km
+    inputs_features = {}
+    st.header("Input values for listed price and km")
+    inputs_features['listed_price'] = st.number_input(
+        "Enter listed price:",
+        min_value=numerical_ranges['listed_price'][0],
+        max_value=numerical_ranges['listed_price'][1],
+        value=st.session_state.inputs['listed_price']
+    )
+    # Sliders for other numerical features
+    st.header("Select values for other numerical features")
+    for feature in numerical_features:
+        if feature not in ['listed_price', 'km']:
+            min_value, max_value = numerical_ranges[feature]
+            inputs_features[feature] = st.slider(
+                f"Select {feature}:",
+                min_value,
+                max_value,
+                st.session_state.inputs[feature]
+            )
+
+    # Dropdowns for categorical features
+    st.header("Select values for categorical features")
+
+    for feature in categorical_features:
+        unique_values = cars_data[feature].unique().tolist()
+        inputs_features[feature] = st.selectbox(
+            f"Select {feature}:",
+            unique_values,
+            index=unique_values.index(st.session_state.inputs[feature])
+        )
+
+    submitted = st.form_submit_button("Show")
+
+if submitted:
+    st.subheader("Recommendations")
+    cars_recommendation = hybrid_model(inputs_features)
+
+    if cars_recommendation.shape[0] == 0 : 
+        st.write("No Recommendation Available")
+
+    for idx, row in cars_recommendation.iterrows():
+        with st.expander(f"{row['model']}"):
+            # Display each feature and its value for the current row
+
+            car_link_dict = ast.literal_eval(row['images'])  # Convert the string representation of the list to an actual list
+            if car_link_dict[0]['img'] != "":
+                car_link = car_link_dict[0]['img']
+                st.image(car_link, caption=row['model'], use_column_width=True)
+
+            st.write(f"Car image link : {car_link}")
+
+            for feature in overall_features:
+                st.write(f"**{feature}:** {row[feature]}")
---
+++ HybridModel.py
@@ -0,0 +1,103 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import NearestNeighbors
+from sklearn.decomposition import TruncatedSVD
+import numpy as np
+
+# Load the dataset
+cars_data = pd.read_csv(r"D:\Car4U\Dataset\cars_data_clean.csv")
+
+# Select relevant features for recommendation
+features = [
+    'listed_price', 'myear', 'body', 'transmission', 'fuel', 'km',
+    'Engine Type', 'Length', 'Width', 'Height', 'Max Power Delivered', 'Color' ,
+    'model' , 'images'
+]
+
+cars_required_data = cars_data[features]
+
+# Define numerical and categorical features
+# numerical_features = ['listed_price', 'myear', 'km', 'Length', 'Width', 'Height', 'Max Power Delivered']
+# categorical_features = ['body', 'transmission', 'fuel', 'Engine Type', 'Color']
+numerical_features = ['listed_price', 'myear', 'Length',]
+categorical_features = ['body', 'transmission', 'fuel', 'Color']
+
+features_to_be_used = numerical_features + categorical_features
+
+car_data = cars_data[features_to_be_used]
+
+# Ensure there are no missing values and duplicates
+car_data = car_data.dropna()
+car_data = car_data.drop_duplicates()
+
+# Preprocessing pipeline
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', Pipeline(steps=[
+            ('scaler', StandardScaler())
+        ]), numerical_features),
+        ('cat', Pipeline(steps=[
+            ('encoder', OneHotEncoder(handle_unknown='ignore'))
+        ]), categorical_features)
+    ]
+)
+
+
+# Function to get recommendations based on KNN (content-based)
+def get_knn_recommendations(user_transformed, knn_model, n_neighbors=10):
+    distances, indices = knn_model.kneighbors(user_transformed, n_neighbors=n_neighbors)
+    return indices.flatten()
+
+# Function to get recommendations based on collaborative filtering
+def get_collaborative_recommendations(user_transformed_svd, X_svd, n_neighbors=10):
+    distances = np.linalg.norm(X_svd - user_transformed_svd, axis=1)
+    indices = np.argsort(distances)[:n_neighbors]
+    return indices
+
+# Function to get hybrid recommendations
+def get_hybrid_recommendations(user_transformed, user_transformed_svd, X_svd, knn_model, n_neighbors=10):
+    knn_indices = get_knn_recommendations(user_transformed, knn_model, n_neighbors)
+    collab_indices = get_collaborative_recommendations(user_transformed_svd, X_svd, n_neighbors)
+    combined_indices = np.unique(np.concatenate((knn_indices, collab_indices)))
+    return combined_indices
+
+
+# Preprocess user input
+def preprocess_user_input(user_data):
+    user_df = pd.DataFrame([user_data])
+    user_transformed = preprocessor.transform(user_df)
+    return user_transformed
+
+def hybrid_model(user_data):
+    car_filtered_data = car_data[(car_data['body'] == user_data['body']) & (car_data['fuel'] == user_data['fuel'])]
+    cars_filtered_required_data = cars_required_data[(cars_required_data['body'] == user_data['body']) & (cars_required_data['fuel'] == user_data['fuel'])]
+
+    if(cars_filtered_required_data.shape[0] <= 10):
+        return cars_filtered_required_data
+
+    X = preprocessor.fit_transform(car_filtered_data)
+
+    # Dynamically set the number of components for SVD
+    n_components = min(50, X.shape[1])
+
+    # Fit the KNN model for content-based filtering
+    knn = NearestNeighbors(n_neighbors=10, algorithm='auto').fit(X)
+
+    # Collaborative Filtering using Matrix Factorization
+    svd = TruncatedSVD(n_components=n_components, random_state=42)
+    X_svd = svd.fit_transform(X)
+
+    user_transformed = preprocess_user_input(user_data)
+    
+    # Applying SVD transformation to the user input
+    user_transformed_svd = svd.transform(user_transformed)
+    
+    hybrid_indices = get_hybrid_recommendations(user_transformed, user_transformed_svd, X_svd, knn, n_neighbors=10)
+
+    recommended_cars = cars_filtered_required_data.iloc[hybrid_indices]
+
+    return recommended_cars
+
+
